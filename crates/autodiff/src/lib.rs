1e21386a026ff626bf4257d281955a66b649a0a7
reyalka <140703515+reyalka@users.noreply.github.com>
2025-12-25 09:48:07 +0900

    feat: enhance autodiff functionality with Dual struct and multiplication support

--- crates/autodiff/src/lib.rs
+++ crates/autodiff/src/lib.rs
@@ -1,8 +1,34 @@
-pub fn diff<T>(f: T, val: f64) -> (f64, f64)
+use std::ops;
+
+#[derive(Debug, Clone, Copy)]
+struct Dual {
+    real: f64,
+    dual: f64,
+}
+
+impl Dual {
+    fn new(real: f64) -> Self {
+        return Self { real, dual: 1.0 };
+    }
+}
+impl ops::Mul for Dual {
+    type Output = Dual;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // (a + bε)(c + dε) = ac + (ad + bc)ε
+        Self {
+            real: self.real * rhs.real,
+            dual: (self.real * rhs.dual + self.dual * rhs.real),
+        }
+    }
+}
+
+fn diff<T>(f: T, val: f64) -> (f64, f64)
 where
-    T: Fn(f64) -> f64,
+    T: Fn(Dual) -> Dual,
 {
-    return (val, 1.0);
+    let result = f(Dual::new(val));
+    (result.real, result.dual)
 }
 
 #[cfg(test)]
@@ -15,5 +41,13 @@ mod tests {
 
         assert_eq!(y, 3.0);
         assert_eq!(dy, 1.0);
+    }
+
+    #[test]
+    fn diff_of_square_funciton() {
+        let (y, dy) = diff(|x| x * x, 3.0);
+
+        assert_eq!(y, 9.0);
+        assert_eq!(dy, 6.0);
     }
 }
